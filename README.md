d09d65249d8859433e12943cb295e31db2763a2d
isacmoreira10e <isacmoreira547@gmail.com>
2023-04-13 00:26:30 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+1-Três botões para adicionar uma div em status aberto, em andamento ou finalizado;
+2-Três arrays de objetos para título e conteúdo da task;
+3-Três divs para mostrar o índice de criação das tasks;
+4-Deixar responsivo;
+5-Um botão em cada modal excluir a tasks desejada;
+6- Fazer alguma função para editar a tasks desejada;
+7-Marcha na boneca;
--- script.js
+++ script.js
@@ -1,108 +1,210 @@
-let modal = document.querySelector('#displayModal')
-let btn = document.querySelector('#myBtn')
-let spanClose = document.querySelector('#close')
-let displayOpenTasks = document.querySelector('#displayOpenTasks')
-let displayProgressTasks = document.querySelector('#displayProgressTasks')
-let displayFinishTasks = document.querySelector('#displayFinishTasks')
+function dateAndHour() {
+    let dataModalOpen = new Date()
+    document.querySelector('#dateTimeModalOpen').innerHTML = dataModalOpen.toLocaleDateString()
 
+    let hourModalOpen = new Date()
+    document.querySelector('#hourTimeModalOpen').innerHTML = hourModalOpen.toLocaleTimeString()
 
+    let dataModalProgress = new Date()
+    document.querySelector('#dateTimeModalProgress').innerHTML = dataModalProgress.toLocaleDateString()
 
-btn.onclick = function () {
-    modal.style.display = "block"
+    let hourModalProgress = new Date()
+    document.querySelector('#hourTimeModalProgress').innerHTML = hourModalProgress.toLocaleTimeString()
+
+    let dataModalFinish = new Date()
+    document.querySelector('#dateTimeModalFinish').innerHTML = dataModalFinish.toLocaleDateString()
+
+    let hourModalFinish = new Date()
+    document.querySelector('#hourTimeModalFinish').innerHTML = hourModalFinish.toLocaleTimeString()
 }
 
-spanClose.onclick = function () {
-    modal.style.display = "none"
+
+function createDivOpenTasks() {
+    createModalOpen()
+    dateAndHour()
 }
 
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none"
+let indexOpenTasks = document.querySelector('#indexOpenTasks')
+let tasksOpenDiv = document.querySelector('#tasksOpenDiv')
+let objectTasksOpen = []
+
+function ButtonAddOpenTasks() {
+    let titleOpenContent = document.querySelector('#titleOpenContent')
+    let tasksOpenContent = document.querySelector('#tasksOpenContent')
+
+    let itemTasksOpen = {
+        titleOpenContent: titleOpenContent.value,
+        tasksOpenContent: tasksOpenContent.value
+    }
+
+    objectTasksOpen.push(itemTasksOpen)
+
+    let newDivOpenTasks = ''
+    for (let index = 0; index < objectTasksOpen.length; index++) {
+        const tasksOpenElementHtml = `<div class="tasksOpenDiv" id="tasksOpenDiv">
+        <br>
+        <div class="taskOpenDivContent">
+            <a class="taskContent"> ${objectTasksOpen[index].tasksOpenContent} </a>
+        </div>
+        <div class="titleOpenDivContent">
+            <a>
+             <img src="Vector (3).png" class="vector3">
+            ${objectTasksOpen[index].titleOpenContent}
+                </a>
+        </div>
+    </div>`
+        newDivOpenTasks += tasksOpenElementHtml
+        indexOpenTasks.innerHTML = index + 1
+    }
+
+    tasksOpenDiv.innerHTML = newDivOpenTasks
+    document.querySelector('#titleOpenContent').value = ''
+    document.querySelector('#tasksOpenContent').value = ''
+}
+
+function createModalOpen() {
+    let modalOpen = document.querySelector('#modalOpenTasks')
+    let btnModalOpen = document.querySelector('#btnModalOpen')
+    let span = document.getElementsByClassName("btnCloseOpen")[0]
+    btnModalOpen.onclick = function () {
+        modalOpen.style.display = "block"
+    }
+
+    span.onclick = function () {
+        modalOpen.style.display = "none"
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modalOpen) {
+            modalOpen.style.display = "none"
+        }
+    }
+
+}
+
+function createModalProgress() {
+    let modalProgress = document.querySelector('#modalProgressTasks')
+    let btnmodalProgress = document.querySelector('#btnModalProgress')
+    let span = document.getElementsByClassName("btnCloseProgress")[0]
+    btnmodalProgress.onclick = function () {
+        modalProgress.style.display = "block"
+    }
+
+    span.onclick = function () {
+        modalProgress.style.display = "none"
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modalProgress) {
+            modalProgress.style.display = "none"
+        }
+    }
+
+}
+
+function CreateDivProgress() {
+    createModalProgress()
+    dateAndHour()
+}
+
+let indexProgressTask = document.querySelector('#indexProgressTask')
+let taskProgressDiv = document.querySelector('#taskProgressDiv')
+let objectTasksProgress = []
+function buttonAddProgressTasks() {
+    let titleProgressContent = document.querySelector('#titleProgressContent')
+    let tasksProgressContent = document.querySelector('#tasksProgressContent')
+
+    let itemTasksProgress = {
+        titleProgressContent: titleProgressContent.value,
+        tasksProgressContent: tasksProgressContent.value
+    }
+
+    objectTasksProgress.push(itemTasksProgress)
+
+    let newDivProgressTasks = ''
+    for (let i = 0; i < objectTasksProgress.length; i++) {
+        const tasksProgressHtmlElement = `<div class="tasksProgressDiv">
+    <br>
+    <div class="taskProgressDivContent">
+        <a class="taskContent">
+        ${objectTasksProgress[i].tasksProgressContent} </a>
+    </div>
+    <div class="titleProgressDivContent">
+        <a>
+            <img src="Vector (3).png" class="vector3">
+        ${objectTasksProgress[i].titleProgressContent}
+            </a>
+    </div>
+</div>`
+        newDivProgressTasks += tasksProgressHtmlElement
+        indexProgressTask.innerHTML = i + 1
+    }
+    taskProgressDiv.innerHTML = newDivProgressTasks
+
+    document.querySelector('#titleProgressContent').value = ''
+    document.querySelector('#tasksProgressContent').value = ''
+}
+
+
+function createModalFinish() {
+    let modalFinish = document.querySelector('#modalFinishTasks')
+    let btnModalFinish = document.querySelector('#btnModalFinish')
+    let span = document.getElementsByClassName("btnCloseFinish")[0]
+    btnModalFinish.onclick = function () {
+        modalFinish.style.display = "block"
+    }
+
+    span.onclick = function () {
+        modalFinish.style.display = "none"
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modalFinish) {
+            modalFinish.style.display = "none"
+        }
     }
 }
 
-
-let data = new Date()
-document.querySelector('#dateTime').innerHTML = data.toLocaleDateString()
-
-let hour = new Date()
-document.querySelector('#hourTime').innerHTML = hour.toLocaleTimeString()
-
-
-let titleTasks = document.querySelector('#titleTasks')
-let tasks = document.querySelector('#tasks')
-let arrayTasksOpen = []
-
-function addTasks() {
-    let itemTasks = {
-        title: titleTasks.value,
-        tasks: tasks.value
-    }
-
-    arrayTasksOpen.push(itemTasks)
-}
-
-
-
-function btnOpen() {
-    addTasks()
-    let newDivTasksOPen = ''
-    for (let i = 0; i < arrayTasksOpen.length; i++) {
-        const divTasksOpen = `<div>
-        <img src="Vector (4).png">
-        <p>Em aberto  ${[i + 1]}</p>
-        <img src="Vector (3).png">
-        ${arrayTasksOpen[i].title} <br>
-        <p>Sua tarefa</p> <hr>
-        ${arrayTasksOpen[i].tasks}
-        </div>`
-        newDivTasksOPen += divTasksOpen
-    }
-    displayOpenTasks.innerHTML = newDivTasksOPen
+function createDivFinish() {
+    createModalFinish()
+    dateAndHour()
 
 }
 
+let objectTasksFinish = []
 
-function btnProgress() {
-    let taskProgress = ''
-    for (let i = 0; i < arrayTasksOpen.length; i++) {
-        const divTasksProgress = `<div>
-        <img src="Vector (4).png">
-        <p>Em andamento  ${[i + 1]}</p>
-        <img src="Vector (3).png">
-        ${arrayTasksOpen[i].title} <br>
-        <p>Sua tarefa</p> <hr>
-        ${arrayTasksOpen[i].tasks}
-        </div>`
-        taskProgress += divTasksProgress
+function buttonAddFinishTasks() {
+    let titleFinishContent = document.querySelector('#titleFinishContent')
+    let tasksFinishContent = document.querySelector('#tasksFinishContent')
+
+    let itemTasksFinish = {
+        titleFinishContent: titleFinishContent.value,
+        tasksFinishContent: tasksFinishContent.value
     }
 
-    displayProgressTasks.innerHTML = taskProgress
-    addTasks()
-}
+    objectTasksFinish.push(itemTasksFinish)
 
-
-function btnFinish() {
-    let taskFinish = ''
-    for (let i = 0; i < arrayTasksOpen.length; i++) {
-        const divTasksOpen = `<div>
-        <img src="Vector (4).png">
-        <p>Finalizado  ${[i + 1]}</p>
-        <img src="Vector (3).png">
-        ${arrayTasksOpen[i].title} <br>
-        <p>Sua tarefa</p> <hr>
-        ${arrayTasksOpen[i].tasks}
-        </div>`
-        taskFinish += divTasksOpen
+    let indexFinishTask = document.querySelector('#indexFinishTask')
+    let taskFinishDiv = document.querySelector('#taskFinishDiv')
+    let newDivProgressTasks = ''
+    for (let indicator = 0; indicator < objectTasksFinish.length; indicator++) {
+        const tasksFinishHtmlElement = ` <div class="tasksFinishDiv">
+                    <br>
+                    <div class="taskFinishDivContent">
+                        <a class="taskContent">${objectTasksFinish[indicator].tasksFinishContent}
+                        </a>
+                    </div>
+                    <div class="titleFinishDivContent">
+                        <a>
+                            <img src="Vector (3).png" class="vector3"> ${objectTasksFinish[indicator].titleFinishContent}</a>
+                    </div>
+                </div>`
+        newDivProgressTasks += tasksFinishHtmlElement
+        indexFinishTask.innerHTML = indicator + 1
     }
-    displayFinishTasks.innerHTML = taskFinish
+
+    taskFinishDiv.innerHTML = newDivProgressTasks
+    document.querySelector('#titleFinishContent').value = ''
+    document.querySelector('#tasksFinishContent').value = ''
 }
-
-
-
-
-
-
-
-
 
